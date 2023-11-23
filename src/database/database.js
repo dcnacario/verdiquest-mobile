084fa982c00a95039f6878ab3aa16c380e23bca1
Danmar Cornelio Nacario <dcnacario08@gmail.com>
2023-11-24 03:34:40 +0800

    Adding git.ignore for database

--- src/database/database.js
+++ src/database/database.js
@@ -1,10 +1,10 @@
-const mysql = require('mysql2/promise');
+const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'verdi_db',
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "verdiquest",
 });
 
 module.exports = pool;
